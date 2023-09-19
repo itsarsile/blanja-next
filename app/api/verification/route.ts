2a8fad5219fc31c0249ae3248dfd7c58b60bd390
Arsile <ajiprio@hotmail.com>
2023-09-19 23:58:19 +0700

    add: verification mode

--- app/api/verification/route.ts
+++ app/api/verification/route.ts
@@ -1,22 +1,41 @@
 import { users } from "@/drizzle/schema";
 import db from "@/src/db";
+import crypto from "crypto";
 import { sendEmail } from "@/src/utils/nodemailer";
+import { and, eq, sql } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   try {
-    const userId = request.nextUrl.searchParams
-    console.log("🚀 ~ file: route.ts:7 ~ GET ~ userId:", userId)
+    const emailParam = request.nextUrl.searchParams.get("email");
+    const verificationCode =
+      request.nextUrl.searchParams.get("verificationCode");
 
-    // const res = await sendEmail({
-    //   to: "ajiprio01@gmail.com",
-    //   html: "hello",
-    //   subject: "Whats'up",
-    // });
-    // console.log(res);
-    return NextResponse.json({ message: "Send email successfully!" });
+    if (!emailParam || !verificationCode) {
+      return NextResponse.json({ message: "Bad Request" }, { status: 400 });
+    }
+
+
+    const [verifiedUser] = await db
+      .update(users)
+      .set({
+        verified: true,
+      })
+      .where(
+        and(
+          eq(users.email, emailParam),
+          eq(users.verificationCode, verificationCode)
+        )
+      ).returning({ verified: users.verified });
+    console.log("🚀 ~ file: route.ts:34 ~ GET ~ userVerificationCode:", verifiedUser)
+
+    if (!verifiedUser) {
+      return NextResponse.json({ message: "Bad Request" }, { status: 400 });
+    }
+
+    return NextResponse.json({ message: "Email verified successfully!" });
   } catch (error) {
-    console.log("🚀 ~ file: route.ts:14 ~ GET ~ error:", error)
+    console.log("🚀 ~ file: route.ts:14 ~ GET ~ error:", error);
     return NextResponse.json({ message: "Error sending email" });
   }
 }
